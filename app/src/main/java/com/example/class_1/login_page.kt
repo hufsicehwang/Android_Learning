0ad757c2784bc1ddcf36d8268683959838c47cea
Hansik Hwang <sksmsgkstlr@naver.com>
2020-07-06 20:04:53 +0900

    setOnClickListener/ setText

--- app/src/main/java/com/example/class_1/login_page.kt
+++ app/src/main/java/com/example/class_1/login_page.kt
@@ -2,10 +2,16 @@ package com.example.class_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_login_page.*
 
 class login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+        //val btnlogin : Button = findViewById(R.id.btn_login)
+        btn_login.setOnClickListener {
+            btn_login.setText("로그인 중 입니다.")
+        }
     }
 }
